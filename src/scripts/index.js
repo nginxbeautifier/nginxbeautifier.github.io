dd7375cd1a406a0af2aeaa33922ff5eb32d574af
Matvii Hodovaniuk <mhodovaniuk@liveperson.com>
2021-03-09 17:57:52 +0200

    Add simple functionality and style

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,16 @@
+import Beautify from 'nginxbeautify';
 import '../styles/index.scss';
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-console.log('NGINX beautifier');
+const beautifier = new Beautify({ spaces: 2 });
+const textarea = document.getElementById('textarea');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  textarea.value = beautifier.parse(textarea.value);
+  return false;
+});
